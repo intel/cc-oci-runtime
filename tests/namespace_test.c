b46b43dfd130ec11488b564e7f4cba1cb58b4812
James Hunt <james.o.hunt@intel.com>
2016-07-14 15:53:03 +0100

    Fix erroneous errno assignment in namespace test.
    Signed-off-by: James Hunt <james.o.hunt@intel.com>

--- tests/namespace_test.c
+++ tests/namespace_test.c
@@ -160,7 +160,7 @@ START_TEST(test_clr_oci_ns_setup) {
 		 * can't call setns due to insufficient privs.
 		 */
 		ck_assert (! clr_oci_ns_setup (&config));
-		ck_assert (errno = EPERM);
+		ck_assert (errno == EPERM);
 
 		ck_assert (! g_remove (tmpdir));
 
