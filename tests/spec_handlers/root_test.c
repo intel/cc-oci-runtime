d703bcbb9cc907449ead69da861cbdd5893ea437
James Hunt <james.o.hunt@intel.com>
2016-07-14 17:03:34 +0100

    Add test to ensure non-directory root.path is caught.
    Signed-off-by: James Hunt <james.o.hunt@intel.com>

--- tests/spec_handlers/root_test.c
+++ tests/spec_handlers/root_test.c
@@ -38,12 +38,13 @@ extern struct spec_handler root_spec_handler;
 * - readonly
 */
 static struct spec_handler_test tests[] = {
-	{ TEST_DATA_DIR "/root-no-path.json"        , false },
-	{ TEST_DATA_DIR "/root-no-readonly.json"    , true  },
-	{ TEST_DATA_DIR "/root-readonly-false.json" , true  },
-	{ TEST_DATA_DIR "/root-path-enoent.json"    , false },
-	{ TEST_DATA_DIR "/root-path-invalid.json"   , false },
-	{ TEST_DATA_DIR "/root.json"                , true  },
+	{ TEST_DATA_DIR "/root-no-path.json"         , false },
+	{ TEST_DATA_DIR "/root-no-readonly.json"     , true  },
+	{ TEST_DATA_DIR "/root-readonly-false.json"  , true  },
+	{ TEST_DATA_DIR "/root-path-enoent.json"     , false },
+	{ TEST_DATA_DIR "/root-path-invalid.json"    , false },
+	{ TEST_DATA_DIR "/root-path-wrong-type.json" , false },
+	{ TEST_DATA_DIR "/root.json"                 , true  },
 	{ NULL, false },
 };
 
@@ -51,21 +52,31 @@ static struct spec_handler_test tests[] = {
 START_TEST(test_root_handle_section) {
 	gchar *tmpdir = g_dir_make_tmp (NULL, NULL);
 
-	/* XXX: the upper-case value must match that in the test json files!
-	*/
+	/* XXX: the upper-case value must match that in the test json
+	 * files!
+	 */
 	gchar *root_dir = g_build_path ("/", tmpdir, "ROOTFS", NULL);
+
+	/* Used to test that the spec handler detects the file type is
+	 * incorrect (since it should be a directory)
+	 */
+	g_autofree gchar *root_file = g_build_path ("/", tmpdir,
+            "ROOTFILE", NULL);
 
 	/* create fake rootfs directory */
 	ck_assert (! g_mkdir (root_dir, 0x750));
 
-	/* move to parent directory (as the runtime does, to allow it to find the rootfs
-	 * below it).
+	ck_assert (g_file_set_contents (root_file, "", -1, NULL));
+
+	/* move to parent directory (as the runtime does, to allow it to
+	 * find the rootfs below it).
 	 */
 	ck_assert (! g_chdir (tmpdir));
 
 	test_spec_handler(&root_spec_handler, tests);
 
 	/* clean up */
+	ck_assert (! g_remove (root_file));
 	ck_assert (! g_remove (root_dir));
 	ck_assert (! g_remove (tmpdir));
 
