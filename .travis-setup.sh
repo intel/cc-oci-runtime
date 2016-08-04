bf0304f16b763745b3f6361c13319a90a18cbcd1
Damien Lespiau <damien.lespiau@intel.com>
2016-08-04 16:50:59 +0100

    travis: Make sure the setup happens without any error
    Signed-off-by: Damien Lespiau <damien.lespiau@intel.com>

--- .travis-setup.sh
+++ .travis-setup.sh
@@ -17,7 +17,7 @@
 #  along with this program; if not, write to the Free Software
 #  Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
 
-set -x
+set -e -x
 
 glib_version=2.46.2
 json_glib_version=1.2.2
