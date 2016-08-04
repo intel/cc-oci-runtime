4641b5c4b9411c3a89dac84721faf1a3f3ef84a3
Damien Lespiau <damien.lespiau@intel.com>
2016-08-04 16:32:18 +0100

    travis: Trace what the setup script is doing
    It makes understanding the logs a lot easier.
    
    Signed-off-by: Damien Lespiau <damien.lespiau@intel.com>

--- .travis-setup.sh
+++ .travis-setup.sh
@@ -17,6 +17,8 @@
 #  along with this program; if not, write to the Free Software
 #  Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
 
+set -x
+
 glib_version=2.49.4
 json_glib_version=1.2.2
 check_version=0.10.0
