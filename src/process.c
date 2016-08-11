16aa19a4552b50a6d15811e7b7362cdc1b56a76b
Damien Lespiau <damien.lespiau@intel.com>
2016-08-11 10:52:29 +0100

    process: Comment out unreachable code
    It seems some code has been left half-done, waiting for more
    developments. However, clang is warning about unreachable code and we're
    trying to be warning free, so comment out the unreachable code.
    
    Signed-off-by: Damien Lespiau <damien.lespiau@intel.com>

--- src/process.c
+++ src/process.c
@@ -199,6 +199,7 @@ cc_oci_setup_child (struct cc_oci_config *config)
  * \note \p exit_code may be \c NULL to avoid returning the
  * child exit code.
  */
+#if 0
 static void
 cc_oci_child_watcher (GPid pid, gint status,
 		gpointer exit_code) {
@@ -212,6 +213,7 @@ cc_oci_child_watcher (GPid pid, gint status,
 
 	g_main_loop_quit (main_loop);
 }
+#endif
 
 /*!
  * Close spawned hook and stop the hook loop.
@@ -999,17 +1001,19 @@ cc_oci_vm_connect (struct cc_oci_config *config,
 		int argc,
 		char *const argv[]) {
 	gchar     **args = NULL;
-	GError     *err = NULL;
 	gboolean    ret = false;
-	GPid        pid;
 	guint       args_len = 0;
-	gint        exit_code = -1;
 	gboolean    cmd_is_just_shell = false;
+#if 0
+	GError     *err = NULL;
+	GPid        pid;
+	gint        exit_code = -1;
 	GSpawnFlags flags = (G_SPAWN_CHILD_INHERITS_STDIN |
 			     /* XXX: required for g_child_watch_add! */
 			     G_SPAWN_DO_NOT_REAP_CHILD |
 			     G_SPAWN_SEARCH_PATH);
 	guint i;
+#endif
 
 	g_assert (config);
 	g_assert (argc);
@@ -1070,11 +1074,6 @@ cc_oci_vm_connect (struct cc_oci_config *config,
 	// FIXME: replace with proper connection string once networking details available.
 #if 0
 	args[1] = g_strdup_printf (ip_address);
-#else
-	g_critical ("not implemented yet");
-	ret = false;
-	goto out;
-#endif
 
 	/* append argv to the end of args */
 	if (argc) {
@@ -1131,6 +1130,10 @@ cc_oci_vm_connect (struct cc_oci_config *config,
 	}
 
 	ret = true;
+#else
+	g_critical ("not implemented yet");
+	ret = false;
+#endif
 
 out:
 	if (args) {
