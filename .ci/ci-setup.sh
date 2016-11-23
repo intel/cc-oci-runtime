84ee231e285f7d2ffdaf42bdc0bfc1ba7080e3d9
James O. D. Hunt <james.o.hunt@intel.com>
2016-11-23 14:24:18 +0000

    CI: Build qemu-lite and gcc if nested kvm is available.
    Download and build qemu-lite and gcc 6 (required for the
    qemu-lite build) if nested kvm is available.
    
    Signed-off-by: James O. D. Hunt <james.o.hunt@intel.com>

--- .ci/ci-setup.sh
+++ .ci/ci-setup.sh
@@ -21,6 +21,22 @@ set -e -x
 
 root=$(cd `dirname "$0"`/..; pwd -P)
 source "$root/versions.txt"
+
+if [ "$SEMAPHORE" = true ]
+then
+    # SemaphoreCI has different environments that builds can run in. The
+    # default environment does not have docker enabled so it is
+    # necessary to specify a docker-enabled build environment on the
+    # semaphoreci.com web site.
+    #
+    # However, currently, the docker-enabled environment does not
+    # provide nested KVM (whereas the default environment does), so
+    # manually enable nested kvm for the time being.
+    sudo rmmod kvm-intel || :
+    sudo sh -c "echo 'options kvm-intel nested=y' >> /etc/modprobe.d/dist.conf" || :
+    sudo modprobe kvm-intel || :
+fi
+
 source $(dirname "$0")/ci-common.sh
 
 #
@@ -58,19 +74,52 @@ export CC=$(which "$CC")
 gnome_dl=https://download.gnome.org/sources
 
 # Install required dependencies to build
-# glib, json-glib, libmnl-dev check and cc-oci-runtime
-sudo apt-get -qq install valgrind lcov uuid-dev pkg-config \
-  zlib1g-dev libffi-dev gettext libpcre3-dev cppcheck \
-  libmnl-dev moreutils
+# glib, json-glib, libmnl-dev, check, gcc, cc-oci-runtime and qemu-lite
+
+pkgs=""
+
+# general
+pkgs+=" pkg-config"
+pkgs+=" gettext"
+pkgs+=" rpm2cpio"
+pkgs+=" valgrind"
+
+# runtime dependencies
+pkgs+=" uuid-dev"
+pkgs+=" cppcheck"
+pkgs+=" libmnl-dev"
+pkgs+=" libffi-dev"
+pkgs+=" libpcre3-dev"
+
+# runtime + qemu-lite
+pkgs+=" zlib1g-dev"
+
+# qemu-lite
+pkgs+=" libpixman-1-dev"
+
+# gcc
+pkgs+=" libcap-ng-dev"
+pkgs+=" libgmp-dev"
+pkgs+=" libmpfr-dev"
+pkgs+=" libmpc-dev"
+
+# code coverage
+pkgs+=" lcov"
+
+# chronic(1)
+pkgs+=" moreutils"
+
+eval sudo apt-get -qq install "$pkgs"
 
 function compile {
 	name="$1"
 	tarball="$2"
 	directory="$3"
+	configure_opts="$4"
 
 	chronic tar -xvf ${tarball}
 	pushd ${directory}
-	chronic	./configure --disable-silent-rules
+	eval chronic ./configure --disable-silent-rules "$configure_opts"
 	chronic make -j5
 	chronic sudo make install
 	popd
@@ -102,5 +151,90 @@ git clone https://github.com/sstephenson/bats.git
 pushd bats
 sudo ./install.sh /usr/local
 popd
+
+if [ "$nested" != "Y" ]
+then
+    popd
+    exit 0
+fi
+
+# build gcc (required for qemu-lite)
+gcc_dir="gcc-${gcc_version}"
+gcc_site="http://mirrors.kernel.org/gnu/gcc/${gcc_dir}"
+gcc_file="gcc-${gcc_version}.tar.bz2"
+gcc_url="${gcc_site}/${gcc_file}"
+curl -L -O "$gcc_url"
+
+gcc_opts=""
+gcc_opts+=" --enable-languages=c"
+gcc_opts+=" --disable-multilib"
+gcc_opts+=" --disable-libstdcxx"
+gcc_opts+=" --disable-bootstrap"
+gcc_opts+=" --disable-nls"
+gcc_opts+=" --prefix=\"/usr/local/${gcc_dir}\""
+
+compile gcc "$gcc_file" "$gcc_dir" "$gcc_opts"
+
+# Use built version of gcc
+export PATH="/usr/local/${gcc_dir}/bin:$PATH"
+export CC="/usr/local/${gcc_dir}/bin/gcc"
+
+# build qemu-lite
+qemu_lite_site="https://github.com/01org/qemu-lite/archive/"
+qemu_lite_file="${qemu_lite_version}.tar.gz"
+qemu_lite_url="${qemu_lite_site}/${qemu_lite_file}"
+qemu_lite_dir="qemu-lite-${qemu_lite_version}"
+
+qemu_lite_opts=""
+qemu_lite_opts+=" --disable-bluez"
+qemu_lite_opts+=" --disable-brlapi"
+qemu_lite_opts+=" --disable-bzip2"
+qemu_lite_opts+=" --disable-curl"
+qemu_lite_opts+=" --disable-curses"
+qemu_lite_opts+=" --disable-debug-tcg"
+qemu_lite_opts+=" --disable-fdt"
+qemu_lite_opts+=" --disable-glusterfs"
+qemu_lite_opts+=" --disable-gtk"
+qemu_lite_opts+=" --disable-libiscsi"
+qemu_lite_opts+=" --disable-libnfs"
+qemu_lite_opts+=" --disable-libssh2"
+qemu_lite_opts+=" --disable-libusb"
+qemu_lite_opts+=" --disable-linux-aio"
+qemu_lite_opts+=" --disable-lzo"
+qemu_lite_opts+=" --disable-opengl"
+qemu_lite_opts+=" --disable-qom-cast-debug"
+qemu_lite_opts+=" --disable-rbd"
+qemu_lite_opts+=" --disable-rdma"
+qemu_lite_opts+=" --disable-sdl"
+qemu_lite_opts+=" --disable-seccomp"
+qemu_lite_opts+=" --disable-slirp"
+qemu_lite_opts+=" --disable-snappy"
+qemu_lite_opts+=" --disable-spice"
+qemu_lite_opts+=" --disable-strip"
+qemu_lite_opts+=" --disable-tcg-interpreter"
+qemu_lite_opts+=" --disable-tcmalloc"
+qemu_lite_opts+=" --disable-tools"
+qemu_lite_opts+=" --disable-tpm"
+qemu_lite_opts+=" --disable-usb-redir"
+qemu_lite_opts+=" --disable-uuid"
+qemu_lite_opts+=" --disable-vnc"
+qemu_lite_opts+=" --disable-vnc-{jpeg,png,sasl}"
+qemu_lite_opts+=" --disable-vte"
+qemu_lite_opts+=" --disable-xen"
+qemu_lite_opts+=" --enable-attr"
+qemu_lite_opts+=" --enable-cap-ng"
+qemu_lite_opts+=" --enable-kvm"
+qemu_lite_opts+=" --enable-virtfs"
+qemu_lite_opts+=" --target-list=x86_64-softmmu"
+qemu_lite_opts+=" --extra-cflags=\"-fno-semantic-interposition -O3 -falign-functions=32\""
+qemu_lite_opts+=" --prefix=/usr/local"
+qemu_lite_opts+=" --datadir=/usr/local/share/qemu-lite"
+qemu_lite_opts+=" --libdir=/usr/local/lib64/qemu-lite"
+qemu_lite_opts+=" --libexecdir=/usr/local/libexec/qemu-lite"
+
+curl -L -O "${qemu_lite_url}"
+
+compile qemu-lite "$qemu_lite_file" \
+"$qemu_lite_dir" "$qemu_lite_opts"
 
 popd
