63a7c23bf596c400928e02a359ccc6e62561d4a1
James O. D. Hunt <james.o.hunt@intel.com>
2016-11-23 13:58:09 +0000

    CI: Move pre-build commands to setup script.
    All the CI pre-build commands now exist in the setup script.
    
    Signed-off-by: James O. D. Hunt <james.o.hunt@intel.com>

--- .travis/travis-setup.sh
+++ .travis/travis-setup.sh
@@ -40,6 +40,11 @@ cp -r vendor/* "$GOPATH/src"
 mkdir -p "$GOPATH/src/github.com/01org/"
 ln -s $PWD "$GOPATH/src/github.com/01org/"
 
+go get github.com/client9/misspell/cmd/misspell
+go get github.com/golang/lint/golint
+go get github.com/sameo/virtcontainers/hyperstart
+go get github.com/sameo/virtcontainers/hyperstart/mock
+
 #
 # Install cc-oci-runtime dependencies
 #
