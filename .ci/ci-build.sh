5ec821fb0506511e347215f5709e18f0008a8a4f
James O. D. Hunt <james.o.hunt@intel.com>
2016-11-23 14:30:30 +0000

    CI: Enable shell tracing in build script.
    Run with "set -x" as is already done for "ci-setup.sh".
    
    Signed-off-by: James O. D. Hunt <james.o.hunt@intel.com>

--- .ci/ci-build.sh
+++ .ci/ci-build.sh
@@ -1,4 +1,4 @@
-#!/bin/bash -e
+#!/bin/bash
 #  This file is part of cc-oci-runtime.
 #
 #  Copyright (C) 2016 Intel Corporation
@@ -16,6 +16,8 @@
 #  You should have received a copy of the GNU General Public License
 #  along with this program; if not, write to the Free Software
 #  Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
+
+set -e -x
 
 source $(dirname "$0")/ci-common.sh
 
