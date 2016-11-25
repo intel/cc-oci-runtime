b4a9cfb77a4a660c73c8c61b2820c08d27e0de64
Damien Lespiau <damien.lespiau@intel.com>
2016-11-25 22:01:55 +0000

    ci: Add gocyclo to the build
    gocyclo computes the cyclomatic complexity in functions and makes sure
    we don't go over a certain number, which would indicate the function is
    too complicated and needs to be broken up.
    
    The number 15 is taken from ciao. It looks like a fine number.
    
    Fixes: https://github.com/01org/cc-oci-runtime/issues/353
    Signed-off-by: Damien Lespiau <damien.lespiau@intel.com>

--- .ci/ci-setup.sh
+++ .ci/ci-setup.sh
@@ -64,6 +64,7 @@ cp -r vendor/* "$GOPATH/src"
 mkdir -p "$GOPATH/src/github.com/01org/"
 ln -s $PWD "$GOPATH/src/github.com/01org/"
 
+go get github.com/fzipp/gocyclo
 go get github.com/client9/misspell/cmd/misspell
 go get github.com/golang/lint/golint
 
