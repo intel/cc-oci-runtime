e7e679cc57815fe9c46d1896a54a9d7c8001009b
Archana Shinde <archana.m.shinde@intel.com>
2017-01-26 01:18:16 +0000

    state: Fix writing uid and gid to the state file.
    The user{} json in the state file was empty as we were not
    writing the uid and gid to the state file.
    
    Signed-off-by: Archana Shinde <archana.m.shinde@intel.com>

--- src/oci.c
+++ src/oci.c
@@ -1568,7 +1568,11 @@ cc_oci_process_to_json(const struct oci_cfg_process *process)
 	json_object_set_string_member (json_process, "cwd", process->cwd);
 	json_object_set_boolean_member (json_process, "terminal",
 			process->terminal);
+
 	json_object_set_object_member (json_process, "user", user);
+	json_object_set_int_member (user, "uid", process->user.uid);
+	json_object_set_int_member (user, "gid", process->user.gid);
+
 	json_object_set_array_member (json_process, "args", args);
 	json_object_set_array_member (json_process, "env", envs);
 	json_object_set_int_member (json_process, "stdio_stream",
