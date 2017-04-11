80426a500593d8a8d3a6818c646548fc0844b64a
Gabriela Cervantes <gabriela.cervantes.tellez@intel.com>
2017-04-11 16:33:12 +0000

    Metrics: Improve readability for iperf2 metrics
    This will separate the client and server commands as well as other
    modifications
    
    Signed-off-by: Gabriela Cervantes <gabriela.cervantes.tellez@intel.com>

--- tests/metrics/network/network-metrics.sh
+++ tests/metrics/network/network-metrics.sh
@@ -32,6 +32,7 @@ port=5001:5001
 image=gabyct/network
 # Measurement time (seconds)
 time=5
+set -e
 
 function setup {
         runtime_docker
@@ -42,53 +43,69 @@ function setup {
 
 # Test single TCP docker->docker iperf bandwidth
 function bandwidth {
-        setup
-        bandwidth_result=$(mktemp)
-        $DOCKER_EXE run -d --name=iperf-server ${image} bash -c "iperf -p ${port} -s" > /dev/null && \
-        server_address=`$DOCKER_EXE inspect --format "{{.NetworkSettings.IPAddress}}" $($DOCKER_EXE ps -ql)` && \
-        $DOCKER_EXE run -ti --rm --name=iperf-client ${image} bash -c "iperf -c ${server_address} -t ${time}" > "$bandwidth_result"
-        total_bandwidth=`cat $bandwidth_result | tail -1 | awk '{print $(NF-1), $NF}'`
-        $DOCKER_EXE rm -f iperf-server > /dev/null
-        rm -f $bandwidth_result
+	setup
+	bandwidth_result=$(mktemp)
+	server_command="iperf -p ${port} -s"
+	$DOCKER_EXE run -d --name=iperf-server ${image} bash -c "${server_command}" > /dev/null
+	server_address=$($DOCKER_EXE inspect --format "{{.NetworkSettings.IPAddress}}" $($DOCKER_EXE ps -ql))
+
+	client_command="iperf -c ${server_address} -t ${time}"
+	$DOCKER_EXE run -ti --rm --name=iperf-client ${image} bash -c "${client_command}" > "$bandwidth_result"
+
+	total_bandwidth=$(cat $bandwidth_result | tail -1 | awk '{print $(NF-1), $NF}')
+	$DOCKER_EXE rm -f iperf-server > /dev/null
+	rm -f $bandwidth_result
 }
 
 # Test single UDP docker->docker jitter
 function jitter {
-        setup
-        jitter_result=$(mktemp)
-        $DOCKER_EXE run -d --name=iperf-server ${image} bash -c "iperf -p ${port} -u -s" > /dev/null && \
-        server_address=`$DOCKER_EXE inspect --format "{{.NetworkSettings.IPAddress}}" $($DOCKER_EXE ps -ql)` && \
-        $DOCKER_EXE run -ti --rm --name=iperf-client ${image} bash -c "iperf -c ${server_address} -u -t ${time}" > "$jitter_result"
-        total_jitter=`cat $jitter_result | tail -1 | awk '{print $(NF-4), $(NF-3)}'`
-        $DOCKER_EXE rm -f iperf-server > /dev/null
-        rm -f $jitter_result
+	setup
+	jitter_result=$(mktemp)
+	server_command="iperf -p ${port} -u -s"
+	$DOCKER_EXE run -d --name=iperf-server ${image} bash -c "${server_command}" > /dev/null
+	server_address=$($DOCKER_EXE inspect --format "{{.NetworkSettings.IPAddress}}" $($DOCKER_EXE ps -ql))
+
+	client_command="iperf -c ${server_address} -u -t ${time}"
+	$DOCKER_EXE run -ti --rm --name=iperf-client ${image} bash -c "${client_command}" > "$jitter_result"
+
+	total_jitter=$(cat $jitter_result | tail -1 | awk '{print $(NF-4), $(NF-3)}')
+	$DOCKER_EXE rm -f iperf-server > /dev/null
+	rm -f $jitter_result
 }
 
 # Test TCP docker->docker bandwidth with multiple network connections (parallelism)
 function bandwidth_multiple_tcp_connections {
-        setup
-        number_tcp_connections=8
-        multiple_tcp_result=$(mktemp)
-        $DOCKER_EXE run -d --name=iperf-server ${image} bash -c "iperf -p ${port} -s" > /dev/null && \
-        server_address=`$DOCKER_EXE inspect --format "{{.NetworkSettings.IPAddress}}" $($DOCKER_EXE ps -ql)` && \
-        $DOCKER_EXE run -ti --rm --name=iperf-client ${image} bash -c "iperf -c ${server_address} -P ${number_tcp_connections} -t ${time}"  > "$multiple_tcp_result"
-        total_multiple_tcp=`cat $multiple_tcp_result | tail -1 | awk '{print $(NF-1), $NF}'`
-        $DOCKER_EXE rm -f iperf-server > /dev/null
-        rm -f $multiple_tcp_result
+	setup
+	number_tcp_connections=8
+	multiple_tcp_result=$(mktemp)
+	server_command="iperf -p ${port} -s"
+	$DOCKER_EXE run -d --name=iperf-server ${image} bash -c "${server_command}" > /dev/null
+	server_address=$($DOCKER_EXE inspect --format "{{.NetworkSettings.IPAddress}}" $($DOCKER_EXE ps -ql))
+
+	client_command="iperf -c ${server_address} -P ${number_tcp_connections} -t ${time}"
+	$DOCKER_EXE run -ti --rm --name=iperf-client ${image} bash -c "${client_command}"  > "$multiple_tcp_result"
+
+	total_multiple_tcp=$(cat $multiple_tcp_result | tail -1 | awk '{print $(NF-1), $NF}')
+	$DOCKER_EXE rm -f iperf-server > /dev/null
+	rm -f $multiple_tcp_result
 }
 
 # Test TCP bandwidth bi-directionally (docker_iperf_server<->docker_iperf_client)
 # Extract bandwidth results for both directions from the one test
 function bidirectional_bandwidth_server_client {
-        setup
-        bidirectional_bandwidth_result=$(mktemp)
-        $DOCKER_EXE run -d --name=iperf-server ${image} bash -c "iperf -p ${port} -s" > /dev/null && \
-        server_address=`$DOCKER_EXE inspect --format "{{.NetworkSettings.IPAddress}}" $($DOCKER_EXE ps -ql)` && \
-        $DOCKER_EXE run -ti --rm --name=iperf-client ${image} bash -c "iperf -c ${server_address} -d -t ${time}" > "$bidirectional_bandwidth_result"
-        total_bidirectional_server_bandwidth=`cat $bidirectional_bandwidth_result | tail -1 | awk '{print $(NF-1), $NF}'`
-        total_bidirectional_client_bandwidth=`cat $bidirectional_bandwidth_result | tail -n 2 | head -1 | awk '{print $(NF-1), $NF}'`
-        $DOCKER_EXE rm -f iperf-server > /dev/null
-        rm -f $bidirectional_bandwidth_result
+	setup
+	bidirectional_bandwidth_result=$(mktemp)
+	server_command="iperf -p ${port} -s"
+	$DOCKER_EXE run -d --name=iperf-server ${image} bash -c "${server_command}" > /dev/null
+	server_address=$($DOCKER_EXE inspect --format "{{.NetworkSettings.IPAddress}}" $($DOCKER_EXE ps -ql))
+
+	client_command="iperf -c ${server_address} -d -t ${time}"
+	$DOCKER_EXE run -ti --rm --name=iperf-client ${image} bash -c "${client_command}" > "$bidirectional_bandwidth_result"
+
+	total_bidirectional_server_bandwidth=$(cat $bidirectional_bandwidth_result | tail -1 | awk '{print $(NF-1), $NF}')
+	total_bidirectional_client_bandwidth=$(cat $bidirectional_bandwidth_result | tail -n 2 | head -1 | awk '{print $(NF-1), $NF}')
+	$DOCKER_EXE rm -f iperf-server > /dev/null
+	rm -f $bidirectional_bandwidth_result
 }
 
 bandwidth
