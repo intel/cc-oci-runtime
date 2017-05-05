891398c791245d31447dd251fd0c9ea3b2a112c6
Geronimo Orozco <geronimo.orozco@intel.com>
2017-05-05 11:21:57 -0500

    build: update_runtime.sh templates and readme
      * templates and control files updated
        - Add autoconf-archive dependency for cc-oci-runtime
          introduced in release 2.1.6
        - Remove files not installed anymore in latest releases
      * Update the debian changelog to reflect the update to 2.1.7
        This file is used to generate debian packages and track the
        changes to OBS.
      * update_runtime.sh
        - Cosmetics fixes to be easy to read
        - Add a variable to push to obs if true
      * Update README
    
    Signed-off-by: Geronimo Orozco <geronimo.orozco@intel.com>

--- data/obs-packaging/cc-oci-runtime/README.md
+++ data/obs-packaging/cc-oci-runtime/README.md
@@ -1,10 +1,18 @@
 #cc-oci-runtime
 
-This source rpm is the clear-containers runtime used to create clear-containers. 
-To update and generate an rpm spec and dsc to create rpm and deb packages, 
-you need to run:
+This directory contains the sources to update rpm specfiles and debian source
+control files to generate ``cc-oci-runtime`` packages for Fedora and Ubuntu.
+
+There is a script to generate specs and dsc automatically, to be pushed to
+Open Build Service (http://openbuildservice.org/) and generate packages for you.
+
 
 ``./update_runtime.sh``
+
+This script will generate/update the sources to create ``cc-oci-runtime`` packages.
+  * cc-oci-runtime.dsc
+  * cc-oci-runtime.spec
+
 
 By default it will generate the specs using the version specified in the file
 ``configure.ac`` at the the root of the repository
--- data/obs-packaging/cc-oci-runtime/update_runtime.sh
+++ data/obs-packaging/cc-oci-runtime/update_runtime.sh
@@ -1,27 +1,60 @@
 #!/bin/bash
-# Automation script to create specs to build cc-oci-runtime.
-# TODO
-# Create an OBS Clear-Builder user to update cc-oci-runtime
+# -*- mode: shell-script; indent-tabs-mode: nil; sh-basic-offset: 4; -*-
+# ex: ts=8 sw=4 sts=4 et filetype=sh
+
+# Automation script to create specs to build cc-oci-runtime
+# Default image to build is the one specified in file configure.ac
+# located at the root of the repository.
+# set -x
 AUTHOR=${AUTHOR:-$(git config user.name)}
 AUTHOR_EMAIL=${AUTHOR_EMAIL:-$(git config user.email)}
-DEFAULT_VERSION=`cat ../../../configure.ac  | grep cc-oci-runtime | grep AC_INIT | tr -d '[](),' | awk '{print $2}'`
-VERSION=${1:-$DEFAULT_VERSION}
-echo "Packaging cc-oci-runtime $VERSION"
-function changelog_update {
-d=`date +"%a, %d %b %Y %H:%M:%S"`
-hash_tag=`git log --oneline --pretty="%H %d" --decorate --tags --no-walk | grep $VERSION| awk '{print $1}'`
-cp debian.changelog debian.changelog-bk
-cat <<< "cc-oci-runtime ($VERSION) stable; urgency=medium
 
-  * Update cc-oci-runtime to: $VERSION: ${hash_tag:0:7} 
+CC_VERSIONS_FILE="../../../configure.ac"
+DEFAULT_VERSION=$(cat ${CC_VERSIONS_FILE} | grep cc-oci-runtime | grep AC_INIT | tr -d '[](),' | awk '{print $2}')
+VERSION=${1:-$DEFAULT_VERSION}
+hash_tag=`git log --oneline --pretty="%H %d" --decorate --tags --no-walk | grep $VERSION| awk '{print $1}'`
+
+OBS_PUSH=${OBS_PUSH:-false}
+
+echo "Running: $0 $@"
+echo "Update cc-oci-runtime $VERSION: ${hash_tag:0:7}"
+
+function changelog_update {
+    d=`date +"%a, %d %b %Y %H:%M:%S"`
+    cp debian.changelog debian.changelog-bk
+    cat <<< "cc-oci-runtime ($VERSION) stable; urgency=medium
+
+  * Update cc-oci-runtime $VERSION ${hash_tag:0:7}
 
  -- $AUTHOR <$AUTHOR_EMAIL>  $d -0600
 " > debian.changelog
-cat debian.changelog-bk >> debian.changelog
-rm debian.changelog-bk
+    cat debian.changelog-bk >> debian.changelog
+    rm debian.changelog-bk
 }
+
 changelog_update $VERSION
+
 sed "s/@VERSION@/$VERSION/g;" cc-oci-runtime.spec-template > cc-oci-runtime.spec
 sed "s/@VERSION@/$VERSION/g;" cc-oci-runtime.dsc-template > cc-oci-runtime.dsc
 sed "s/@VERSION@/$VERSION/g;" _service-template > _service
 sed "s/@HASH_TAG@/$hash_tag/g;" update_commit_id.patch-template > update_commit_id.patch
+
+# Update and package OBS
+if [ "$OBS_PUSH" = true ]
+then
+    osc co home:clearlinux:preview:clear-containers-staging/cc-oci-runtime
+    mv cc-oci-runtime.spec \
+        cc-oci-runtime.dsc \
+        _service \
+        home\:clearlinux\:preview\:clear-containers-staging/cc-oci-runtime/
+    cp debian.changelog \
+        debian.rules \
+        debian.compat \
+        debian.control \
+        debian.postinst \
+        debian.series \
+        *.patch \
+        home\:clearlinux\:preview\:clear-containers-staging/cc-oci-runtime/
+    cd home\:clearlinux\:preview\:clear-containers-staging/cc-oci-runtime/
+    osc commit -m "Update cc-oci-runtime $VERSION: ${hash_tag:0:7}"
+fi
